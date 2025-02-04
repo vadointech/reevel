e8740e098af981bfe264b7df55d8cbd131d596fb
Vlad <vladvsavenko@gmail.com>
2025-02-04 10:22:06 +0200

    create basic

---
+++ apps/pwa/src/app/[locale]/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import { getTranslations } from "next-intl/server";
+
+export default async function Home() {
+
+    const t = await getTranslations();
+
+    return (
+        <div>
+            test
+        </div>
+    );
+}
