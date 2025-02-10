5997c91636aed72538f7f81d95f95841a8442087
Vlad <vladvsavenko@gmail.com>
2025-02-10 13:33:48 +0200

    assembled register page

--- apps/pwa/src/app/[locale]/(auth)/register/_components/carousel/carousel.tsx
+++ apps/pwa/src/app/[locale]/(auth)/register/_components/carousel/carousel.tsx
@@ -2,7 +2,16 @@ import cx from "classnames";
 import styles from "./styles.module.scss";
 import { CarouselSet } from "../carousel-set";
 import { CarouselCard } from "@/components/ui/carousel-card";
-import movie_img from "@/../public/assets/temp/test.jpg";
+import image_1 from "@/../public/assets/temp/carousel1.jpg";
+import image_2 from "@/../public/assets/temp/carousel2.jpg";
+import image_3 from "@/../public/assets/temp/carousel3.jpg";
+import image_4 from "@/../public/assets/temp/carousel4.jpg";
+import image_5 from "@/../public/assets/temp/carousel5.jpg";
+import image_6 from "@/../public/assets/temp/carousel6.jpg";
+import image_7 from "@/../public/assets/temp/carousel7.jpg";
+
+
+
 
 
 
@@ -25,20 +34,20 @@ const CarouselLayout = ({
             {...props}
         >
             <CarouselSet className={styles.first}>
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
+                <CarouselCard img={image_1} />
+                <CarouselCard img={image_2} />
+                <CarouselCard img={image_3} />
             </CarouselSet>
             <CarouselSet className={styles.second}>
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
+                <CarouselCard img={image_4} />
+                <CarouselCard img={image_5} />
+                <CarouselCard img={image_6} />
+                <CarouselCard img={image_7} />
             </CarouselSet>
             <CarouselSet className={styles.third}>
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
-                <CarouselCard img={movie_img} />
+                <CarouselCard img={image_4} />
+                <CarouselCard img={image_1} />
+                <CarouselCard img={image_5} />
             </CarouselSet>    </div>
     );
 };
--- apps/pwa/src/app/[locale]/(auth)/register/page.tsx
+++ apps/pwa/src/app/[locale]/(auth)/register/page.tsx
@@ -1,18 +1,47 @@
 import { getTranslations } from "next-intl/server";
 import { CarouselLayout } from "./_components/carousel";
 import styles from "./styles.module.scss";
-
-
-
+import { Title } from "@/components/ui/title";
+import { Hint } from "@/components/ui/hint/hint.component";
+import { Container } from "@/components/ui/container";
+import { Dots } from "@/components/ui/dots";
+import { Button } from "@/components/ui/button";
+import { GoogleIcon } from "@/components/ui/icons";
+import AppleIcon from "@/components/ui/icons/apple-icon";
 
 export default async function Home() {
 
-    const t = await getTranslations();
-
     return (
-        <div className={styles.test}>
-            <CarouselLayout>
-            </CarouselLayout>
+        <div className={styles.wrapper}>
+            <CarouselLayout />
+            <Container className={styles.auth}>
+                <div className={styles.auth__info}>
+                    <div className={styles.auth__text}>
+                        <Title
+                            align="center"
+                            size="3xl"
+                            weight={500}
+                        >
+                            Make Every Moment Count
+                        </Title>
+                        <Hint>
+                            Easily bring people together. Reevel turns simple moments into lasting memories.
+                        </Hint>
+                    </div>
+                    <Dots />
+                </div>
+
+                <div className={styles.auth__buttons}>
+                    <Button variant="default">
+                        <GoogleIcon />
+                        Sign in with Google
+                    </Button>
+                    <Button variant="primary">
+                        <AppleIcon />
+                        Sign in with Apple
+                    </Button>
+                </div>
+            </Container>
         </div>
     );
 }
