2796f20c674cc4c8e4137b12eeaf390b24ac610d
Vlad <vladvsavenko@gmail.com>
2025-02-17 16:39:27 +0200

    positioned blocks

--- apps/pwa/src/app/[locale]/onboarding/interests/page.tsx
+++ apps/pwa/src/app/[locale]/onboarding/interests/page.tsx
@@ -1,13 +1,10 @@
-'use client'
-
 import { Button, Container } from "@/components/ui";
 
 import styles from "./styles.module.scss";
 import { ProgressBar } from "@/components/shared";
 import { ArrowBack } from "@/components/icons";
 import { OnboardingTextBlock } from "../_components";
-import { InterestCard } from "@/components/shared/interest-card";
-import { useState } from "react";
+import { InterestsSection } from "../_components/interests-section";
 
 export default function Page() {
 
@@ -35,19 +32,6 @@ export default function Page() {
         { name: "Sport", icon: "🥊" },
     ];
 
-    const [selectedInterests, setSelectedInterests] = useState<Record<string, boolean>>({
-        sport: false,
-        music: false,
-        travel: false,
-    });
-
-    const handleInterestChange = (interest: string, isSelected: boolean) => {
-        setSelectedInterests(prev => ({
-            ...prev,
-            [interest]: isSelected,
-        }));
-    };
-
     return (
         <div className={styles.page}>
             <Container>
@@ -66,16 +50,9 @@ export default function Page() {
             </Container>
 
             <Container className={styles.page__interests}>
-                {interests.map((item, i) => (
-                    <InterestCard
-                        icon={item.icon}
-                        text={item.name}
-                        key={i}
-                        selected={selectedInterests[item.name.toLowerCase()] || false}
-                        onChange={(isSelected) => handleInterestChange(item.name.toLowerCase(), isSelected)}
-                    />
-                ))}
+                <InterestsSection interests={interests} />
             </Container>
+
             <Container className={styles.page__buttons}>
                 <Button
                     variant={"primary"}
