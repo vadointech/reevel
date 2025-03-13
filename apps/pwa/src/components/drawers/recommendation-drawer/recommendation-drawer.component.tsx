1f943d0152745307519e7d49daa33696f9e79e98
Vlad <vladvsavenko@gmail.com>
2025-03-13 15:25:03 +0200

    [REVL-33] fix error

--- apps/pwa/src/components/drawers/recommendation-drawer/recommendation-drawer.component.tsx
+++ apps/pwa/src/components/drawers/recommendation-drawer/recommendation-drawer.component.tsx
@@ -32,7 +32,7 @@ export const RecommendationDrawer = ({ }: RecommendationDrawer.Props) => {
                     <div className={styles.drawer}>
                         <div className={styles.drawer__content}>
                             <div className={styles.drawer__content__input}>
-                                <Input inputSize="default" variant="rounded" placeholder="Search events" icon={<Search />} />
+                                <Input variant="rounded" placeholder="Search events" icon={<Search />} />
                                 <Avatar size={40} variant="profile" type="custom" />
                             </div>
 
