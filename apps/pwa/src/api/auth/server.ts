bc3f56d4e58703317b3ea6255e331728be387115
Oleh <nadvorniyoleh@icloud.com>
2025-09-16 19:01:26 +0300

    [REVL-69] Refactored Auth flow; added `getGoogleOAuthLink` handler, updated Login page logic, and adjusted carousel styles.

--- apps/pwa/src/api/auth/server.ts
+++ apps/pwa/src/api/auth/server.ts
@@ -2,7 +2,13 @@
 
 import { getAccessToken } from "@/api/server";
 
+import * as GetGoogleOAuthLink from "./get-google-oauth-link";
 import * as Logout from "./logout";
+
+export async function getGoogleOAuthLink() {
+    const response = await GetGoogleOAuthLink.getGoogleOAuthLink({})
+    return response.data?.link;
+}
 
 export async function logout() {
     const accessToken = await getAccessToken();
