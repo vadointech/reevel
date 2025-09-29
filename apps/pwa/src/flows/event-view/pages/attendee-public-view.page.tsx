0e93edc7cd88a1ae037940b2348f1b65c1b1b347
Oleh <nadvorniyoleh@icloud.com>
2025-09-29 23:13:58 +0300

    [REVL-72] Added JSON-LD metadata for the Event Public View page.

--- apps/pwa/src/flows/event-view/pages/attendee-public-view.page.tsx
+++ apps/pwa/src/flows/event-view/pages/attendee-public-view.page.tsx
@@ -5,8 +5,11 @@ import { InterestButton, OptionsList, OptionsListItem } from "@/components/ui";
 import { ReviewsSection, ScrollSection, Section } from "@/components/sections";
 import { IconReport } from "@/components/icons";
 
+import { Event, WithContext } from "schema-dts";
+
 import styles from "../styles/event-view-page.module.scss";
 import cx from "classnames";
+import { BASE_URL } from "@/auth.config";
 
 export namespace EventAttendeePublicViewPage {
     export type Props = {
@@ -21,47 +24,76 @@ export async function EventAttendeePublicViewPage({ eventId }: EventAttendeePubl
         return null;
     }
 
+    const jsonLd: WithContext<Event> = {
+        "@context": "https://schema.org",
+        "@type": "Event",
+        name: event.title,
+        startDate: event.startDate.toString(),
+        endDate: event.endDate?.toString(),
+        location: {
+            "@type": "Place",
+            name: event.locationTitle,
+        },
+        image: [event.poster],
+        description: event.description,
+        offers: {
+            "@type": "Offer",
+            url: `${BASE_URL}/discover/event/${event.id}`,
+            price: event.ticketPrice,
+            availability: "https://schema.org/InStock",
+            validFrom: event.startDate.toString(),
+        },
+    };
+
     return (
-        <EventDrawerRoot>
-            <EventDrawerContent event={event}>
-                <div
-                    className={cx(
-                        styles.interests,
-                        styles.gap,
-                    )}
-                >
-                    <ScrollSection size={"small"}>
-                        {
-                            event.interests.map(interest => (
-                                <InterestButton
-                                    key={interest.interestId}
-                                    icon={interest.interest.icon}
-                                >
-                                    { interest.interest.title_uk }
-                                </InterestButton>
-                            ))
-                        }
-                    </ScrollSection>
-                </div>
+        <>
+            <EventDrawerRoot>
+                <EventDrawerContent event={event}>
+                    <div
+                        className={cx(
+                            styles.interests,
+                            styles.gap,
+                        )}
+                    >
+                        <ScrollSection size={"small"}>
+                            {
+                                event.interests.map(interest => (
+                                    <InterestButton
+                                        key={interest.interestId}
+                                        icon={interest.interest.icon}
+                                    >
+                                        { interest.interest.title_uk }
+                                    </InterestButton>
+                                ))
+                            }
+                        </ScrollSection>
+                    </div>
 
-                <ReviewsSection
-                    title={"Rating & reviews"}
-                    cta={"See all"}
-                    rating={4.5}
-                    count={578}
-                    variant={"text-accent"}
-                    className={styles.gap}
-                />
+                    <ReviewsSection
+                        title={"Rating & reviews"}
+                        cta={"See all"}
+                        rating={4.5}
+                        count={578}
+                        variant={"text-accent"}
+                        className={styles.gap}
+                    />
 
-                <Section container>
-                    <OptionsList>
-                        <OptionsListItem
-                            label={"Report"}
-                            contentLeft={<IconReport />}
-                        />
-                    </OptionsList>
-                </Section>
-            </EventDrawerContent>
-        </EventDrawerRoot>
+                    <Section container>
+                        <OptionsList>
+                            <OptionsListItem
+                                label={"Report"}
+                                contentLeft={<IconReport />}
+                            />
+                        </OptionsList>
+                    </Section>
+                </EventDrawerContent>
+            </EventDrawerRoot>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+                }}
+            />
+        </>
     );
 }
